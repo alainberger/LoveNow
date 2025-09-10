f32c93e05afc1e1309527741aa8044e2c5ea1ac0
alainberger <alainberger34000@gmail.com>
2025-09-10 19:46:11 +0200

    Update firebase-config.sample.js

--- js/firebase-config.sample.js
+++ js/firebase-config.sample.js
@@ -1,8 +1,18 @@
-// Copiez ce fichier en firebase-config.js et remplissez vos valeurs Firebase
+// /js/firebase-config.js
+// Valeurs publiques pour le front (ne JAMAIS mettre de secrets ici)
 window.env = {
-  FIREBASE_API_KEY: 'xxx',
-  FIREBASE_AUTH_DOMAIN: 'xxx.firebaseapp.com',
-  FIREBASE_PROJECT_ID: 'xxx',
-  FIREBASE_APP_ID: 'xxx',
-  FIREBASE_STORAGE_BUCKET: 'xxx.appspot.com'
+  // --- Firebase ---
+  FIREBASE_API_KEY:       "AIzaSyB_-cNA8bxqYCYUp3rUZs-VpiP4DX2wn3M",
+  FIREBASE_AUTH_DOMAIN:   "lovenow-officiel.firebaseapp.com",
+  FIREBASE_PROJECT_ID:    "lovenow-officiel",
+  FIREBASE_APP_ID:        "1:896585801571:web:cdde87293291dc1900bd56",
+  FIREBASE_STORAGE_BUCKET:"lovenow-officiel.firebasestorage.app",
+
+  // --- reCAPTCHA (clé SITE publique) ---
+  RECAPTCHA_SITE_KEY:     "6LfjpsErAAAAAExN2IvBq-K475TeJooeNzl9liPD",
+
+  // --- Chat (facultatif) ---
+  // Mets CHAT_PROVIDER="recapchat" et un CHAT_SITE_ID si tu actives le widget
+  CHAT_PROVIDER:          "",
+  CHAT_SITE_ID:           ""
 };
